bac90f598a459e4085ba387d86fbb8f64cdcd599
aaron-at97 <arenas.aaron97@gmail.com>
2022-01-03 18:21:50 +0100

    arreglos equals

--- Test/services/CertificationAuthorityImplTest.java
+++ Test/services/CertificationAuthorityImplTest.java
@@ -3,6 +3,7 @@ package services;
         import data.*;
         import org.junit.jupiter.api.BeforeEach;
         import org.junit.jupiter.api.Test;
+        import publicadministration.exceptions.AnyMobileRegisteredException;
         import services.exceptions.*;
 
         import java.math.BigInteger;
@@ -31,26 +32,25 @@ public class CertificationAuthorityImplTest {
     void sendTest() throws Exception {
 
         assertTrue(agenda.sendPIN(new Nif("78545954N"), new Date(2021-1900, Calendar.APRIL, 6, 17,30)));
-        assertTrue(agenda.sendPIN(new Nif("28148954S"), new Date(2021 - 1900, Calendar.AUGUST, 7, 4, 8)));
 
         assertThrows(NifNotRegisteredException.class, () -> agenda.sendPIN(new Nif("87448231Y"),
                 new Date(2021 - 1900, Calendar.AUGUST, 7, 4, 8)));
 
-        assertThrows(IncorrectValDateException.class, () -> agenda.sendPIN(new Nif("28148954S"),
+        assertThrows(IncorrectValDateException.class, () -> agenda.sendPIN(new Nif("78545954N"),
                 new Date(2021 - 1900, Calendar.SEPTEMBER, 8, 1, 50)));
 
+        assertThrows(AnyMobileRegisteredException.class, () -> agenda.sendPIN(new Nif("28148954S"),
+                new Date(2021 - 1900, Calendar.AUGUST, 7, 4, 8)));
     }
 
     @Test
     void checkPinTest() throws Exception {
         agenda.sendPIN(new Nif("78545954N"), new Date(2021-1900, Calendar.APRIL, 6, 17,30));
-        agenda.sendPIN(new Nif("28148954S"), new Date(2021 - 1900, Calendar.AUGUST, 7, 4, 8));
+
         assertTrue(agenda.checkPIN(new Nif("78545954N"), new PINcode("123")));
-        assertTrue(agenda.checkPIN(new Nif("28148954S"), new PINcode("123")));
+
 
         assertThrows(NotValidPINException.class, () -> agenda.checkPIN(new Nif("78545954N"), new PINcode("568")));
-
-
 
     }
 
@@ -59,34 +59,25 @@ public class CertificationAuthorityImplTest {
 
         assertEquals(1, agenda.checkCredent(new Nif("59168954S"), new Password("S12a3v4652")));
         assertEquals(2, agenda.checkCredent(new Nif("98748978T"), new Password("56835Da6825")));
-        assertEquals(0, agenda.checkCredent(new Nif("78545954N"), null));
-        assertEquals(0, agenda.checkCredent(new Nif("28148954S"), null));
+        assertEquals(0, agenda.checkCredent(new Nif("78545954N"), new Password("Ef22a3v4652")));
 
         assertThrows(NifNotRegisteredException.class, () -> agenda.checkCredent(new Nif("756668954S"), new Password("S12a3v4652")));
         assertThrows(NotValidCredException.class, () -> agenda.checkCredent(new Nif("98748978T"), new Password("14535Da6825")));
-
     }
 
     @Test
     void sendCertfAuthTest() throws Exception {
 
         agenda = new CertificationAuthorityImpl(certDigital);
-        System.out.println(key);
         EncryptedData cipherText = agenda.sendCertfAuth(key);
 
-        System.out.println(key);
-        assertEquals(decrypt.getEncrypted(certDigital.get(key), key) ,agenda.sendCertfAuth(key).getData());
-
-
-        System.out.println("CHIPHER:" + cipherText);
         String decryptedText = decrypt.getDecrypted(new String(cipherText.getData()), decrypt.getPrivateKey());
-        System.out.println("DECRYPTED STRING:" + decryptedText);
+        assertEquals("19874897B", decryptedText);
 
     }
     @Test
     void sendCertfAuthErroresTest() {
 
-        agenda = new CertificationAuthorityImpl(certDigital);
         assertThrows(NotValidCertificateException.class, () -> agenda.sendCertfAuth(new EncryptingKey(BigInteger.ONE)));
         assertThrows(NotValidCertificateException.class, () -> agenda.sendCertfAuth(null));
 
